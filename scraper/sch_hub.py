0631e2d30cf61a9bf20d4a8041bbf89712f08793
ethanc-ec <44012676+ethanc-ec@users.noreply.github.com>
2023-09-03 16:35:21 -0400

    Stuff

---
+++ scraper/sch_hub.py
@@ -0,0 +1,25 @@
+
+def scrape():
+
+    bu_branches = {
+            'khc': 'Kilachand Honors College',
+            'busm': 'Chobanian & Avedisian School of Medicine',
+            'cas': 'College of Arts and Sciences',
+            'com': 'College of Communication',
+            'eng': 'College of Engineering',
+            'cfa': 'College of Fine Arts',
+            'cgs': 'College of General Studies',
+            'sar': 'College of Health & Rehabilitation Sciences: Sargent College',
+            'cds': 'Faculty of Computing & Data Sciences',
+            'gms': 'Graduate Medical Sciences',
+            'grs': 'Graduate School of Arts & Sciences',
+            'sdm': 'Henry M. Goldman School of Dental Medicine',
+            'met': 'Metropolitan College',
+            'questrom': 'Questrom School of Business',
+            'sha': 'School of Hospitality Administration',
+            'law': 'School of Law',
+            'sph': 'School of Public Health',
+            'ssw': 'School of Social Work',
+            'sth': 'School of Theology',
+            'wheelock': 'Wheelock College of Education & Human Development',
+        }
